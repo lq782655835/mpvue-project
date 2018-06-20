46fad1e92a99947ad36f69324cbc369985d81043
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-06-20 16:47:02 +0800

    fix: fix bit point

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -52,17 +52,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {{#lint}}
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {{/lint}}
       {
         test: /\.vue$/,
         loader: 'mpvue-loader',
