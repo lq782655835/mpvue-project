6d8a801308e81b6b1637a120c6daded91cd05929
liaoqiao@corp.netease.com <liaoqiao@corp.netease.com>
2018-06-20 17:00:24 +0800

    fix: add static image

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -52,7 +52,8 @@ module.exports = {
     }],
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'indent': [2, 4, { 'SwitchCase': 1 }]
   },
   globals: {
     App: true,
